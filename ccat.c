be7b521026ff8b276d64ad3e56b152d9a4f84b53
Patrick Brünn <p.bruenn@beckhoff.com>
2014-03-12 17:09:30 +0100

    moved module and pci initialization functions from netdev.c to ccat.c

--- ccat.c
+++ ccat.c
@@ -19,7 +19,11 @@
 */
 
 #include <asm/dma.h>
+#include <linux/etherdevice.h>
+#include <linux/module.h>
+#include <linux/netdevice.h>
 #include "ccat.h"
+#include "netdev.h"
 
 void ccat_bar_free(struct ccat_bar *bar)
 {
@@ -110,3 +114,145 @@ int ccat_dma_init(struct ccat_dma *const dma, size_t channel, void __iomem *cons
 	printk(KERN_INFO "%s: DMA%d mem initialized\n virt:         0x%p\n phys:         0x%llx\n translated:   0x%llx\n pci addr:     0x%08x%x\n memTranslate: 0x%x\n size:         %u bytes.\n", DRV_NAME, channel, dma->virt, (uint64_t)(dma->phys), addr, ioread32(ioaddr + offset + 4), ioread32(ioaddr + offset), memTranslate, dma->size);
 	return 0;
 }
+
+static void ccat_remove_pci(struct ccat_eth_priv *priv)
+{
+	ccat_dma_free(&priv->tx_fifo.dma);
+	ccat_dma_free(&priv->rx_fifo.dma);
+	ccat_bar_free(&priv->bar[2]);
+	ccat_bar_free(&priv->bar[0]);
+	priv->pdev = NULL;
+}
+
+static void ccat_remove_one(struct pci_dev *pdev)
+{
+	struct net_device *const netdev = pci_get_drvdata(pdev);
+	if(netdev) {
+		struct ccat_eth_priv *const priv = netdev_priv(netdev);
+		ccat_eth_remove(netdev);
+		ccat_remove_pci(priv);
+		free_netdev(netdev);
+		pci_disable_device (pdev);
+		printk(KERN_INFO "%s: cleanup done.\n\n", DRV_NAME);
+	}
+}
+
+static int ccat_init_pci(struct ccat_eth_priv *priv)
+{
+	struct pci_dev *pdev = priv->pdev;
+	void __iomem *addr;
+	size_t i;
+	int status;
+	uint8_t revision;
+	uint8_t num_functions;
+	status = pci_enable_device (pdev);
+	if(status) {
+		printk(KERN_INFO "%s: enable CCAT pci device %s failed with %d\n", DRV_NAME, pdev->dev.kobj.name, status);
+		return status;
+	}
+
+	pci_set_master(pdev);
+	status = pci_read_config_byte(pdev, PCI_REVISION_ID, &revision);
+	if(status) {
+		printk(KERN_INFO "%s: read CCAT pci revision failed with %d\n", DRV_NAME, status);
+		return status;
+	}
+
+	/* FIXME upgrade to a newer kernel to get support of dma_set_mask_and_coherent()
+	if (!dma_set_mask_and_coherent(&dev->dev, DMA_BIT_MASK(64))) { */
+	//if (!dma_set_mask(&pdev->dev, DMA_BIT_MASK(64))) {
+	if (!dma_set_mask(&pdev->dev, DMA_BIT_MASK(64))) {
+		printk(KERN_INFO "%s: 64 bit DMA supported.\n", DRV_NAME);
+	/*} else if (!dma_set_mask_and_coherent(&dev->dev, DMA_BIT_MASK(32))) { */
+	} else if (!dma_set_mask(&pdev->dev, DMA_BIT_MASK(32))) {
+		printk(KERN_INFO "%s: 32 bit DMA supported.\n", DRV_NAME);
+	} else {
+		printk(KERN_WARNING "%s: No suitable DMA available.\n", DRV_NAME);
+	}
+
+	if(ccat_bar_init(&priv->bar[0], 0, priv->pdev)) {
+		printk(KERN_WARNING "%s: initialization of bar0 failed.\n", DRV_NAME);
+		return -EIO;
+	}
+
+	if(ccat_bar_init(&priv->bar[2], 2, priv->pdev)) {
+		printk(KERN_WARNING "%s: initialization of bar2 failed.\n", DRV_NAME);
+		return -EIO;
+	}
+
+	num_functions = ioread8(priv->bar[0].ioaddr + 4); /* jump to CCatInfoBlock.nMaxEntries */
+
+	/* find CCATINFO_ETHERCAT_MASTER_DMA function */
+	for(i = 0, addr = priv->bar[0].ioaddr; i < num_functions; ++i, addr += sizeof(priv->info)) {
+		if(CCATINFO_ETHERCAT_MASTER_DMA == ioread16(addr)) {
+			status = ccat_eth_init(priv, addr);
+			break;
+		}
+	}
+	return status;
+}
+
+static int ccat_init_one(struct pci_dev *pdev, const struct pci_device_id *id)
+{
+	struct net_device *netdev;
+	struct ccat_eth_priv *priv;
+
+	netdev = alloc_etherdev(sizeof(*priv));
+	if(!netdev) {
+		printk(KERN_INFO "%s: mem alloc failed.\n", DRV_NAME);
+		return -ENOMEM;
+	}
+	priv = netdev_priv(netdev);
+	priv->pdev = pdev;
+	priv->netdev = netdev;
+	pci_set_drvdata(pdev, netdev);
+
+	/* pci initialization */
+	if(ccat_init_pci(priv)) {
+		printk(KERN_INFO "%s: CCAT pci init failed.\n", DRV_NAME);
+		ccat_remove_one(priv->pdev);
+		return -EIO;
+	}
+	SET_NETDEV_DEV(netdev, &pdev->dev);
+
+	/* complete ethernet device initialization */
+	if(ccat_eth_init_netdev(netdev)) {
+		printk(KERN_INFO "%s: unable to register network device.\n", DRV_NAME);
+		ccat_remove_one(pdev);
+		return -EINVAL;
+	}
+	printk(KERN_INFO "%s: registered %s as network device.\n", DRV_NAME, netdev->name);
+	return 0;
+}
+
+#define PCI_DEVICE_ID_BECKHOFF_CCAT 0x5000
+#define PCI_VENDOR_ID_BECKHOFF 0x15EC
+
+static const struct pci_device_id pci_ids[] = {
+	{ PCI_DEVICE(PCI_VENDOR_ID_BECKHOFF, PCI_DEVICE_ID_BECKHOFF_CCAT) },
+	{ 0, },
+};
+MODULE_DEVICE_TABLE(pci, pci_ids);
+
+static struct pci_driver pci_driver = {
+	.name = DRV_NAME,
+	.id_table = pci_ids,
+	.probe = ccat_init_one,
+	.remove = ccat_remove_one,
+};
+
+static void ccat_exit_module(void) {
+	pci_unregister_driver(&pci_driver);
+}
+
+static int ccat_init_module(void) {
+	BUILD_BUG_ON(sizeof(struct ccat_eth_frame) != sizeof(CCatDmaTxFrame));
+	BUILD_BUG_ON(sizeof(struct ccat_eth_frame) != sizeof(CCatRxDesc));
+	BUILD_BUG_ON(offsetof(struct ccat_eth_frame, data) != offsetof(CCatDmaTxFrame, data));
+	BUILD_BUG_ON(offsetof(struct ccat_eth_frame, data) != offsetof(CCatRxDesc, data));
+	pr_info("%s, %s\n", DRV_DESCRIPTION, DRV_VERSION);
+	return pci_register_driver(&pci_driver);
+}
+
+module_exit(ccat_exit_module);
+module_init(ccat_init_module);
--- netdev.c
+++ netdev.c
@@ -28,6 +28,7 @@
 #include <linux/spinlock.h>
 
 #include "ccat.h"
+#include "netdev.h"
 #include "print.h"
 
 #define TESTING_ENABLED 0
@@ -166,23 +167,10 @@ inline static size_t ccat_eth_priv_read_link_state(const struct ccat_eth_priv *c
 	return (1 << 24) == (ioread32(priv->reg.mii + 0x8 + 4) & (1 << 24));
 }
 
-#define PCI_DEVICE_ID_BECKHOFF_CCAT 0x5000
-#define PCI_VENDOR_ID_BECKHOFF 0x15EC
 
-static const struct pci_device_id pci_ids[] = {
-	{ PCI_DEVICE(PCI_VENDOR_ID_BECKHOFF, PCI_DEVICE_ID_BECKHOFF_CCAT) },
-	{ 0, },
-};
-MODULE_DEVICE_TABLE(pci, pci_ids);
-
-
-static int ccat_eth_init_one(struct pci_dev *pdev, const struct pci_device_id *id);
-static int ccat_eth_init_pci(struct ccat_eth_priv *priv);
 static struct rtnl_link_stats64* ccat_eth_get_stats64(struct net_device *dev, struct rtnl_link_stats64 *storage);
 static int ccat_eth_open(struct net_device *dev);
 static netdev_tx_t ccat_eth_start_xmit(struct sk_buff *skb, struct net_device *dev);
-static void ccat_eth_remove_one(struct pci_dev *pdev);
-static void ccat_eth_remove_pci(struct ccat_eth_priv *priv);
 static int ccat_eth_stop(struct net_device *dev);
 static void ccat_eth_xmit_raw(struct net_device *dev, const char *const data, size_t len);
 
@@ -232,99 +220,22 @@ static struct rtnl_link_stats64* ccat_eth_get_stats64(struct net_device *dev, st
 	return storage;
 }
 
-static int ccat_eth_init_one(struct pci_dev *pdev, const struct pci_device_id *id)
+int ccat_eth_init(struct ccat_eth_priv *const priv, const void __iomem *const addr)
 {
-	struct net_device *netdev;
-	struct ccat_eth_priv *priv;
-	
-	netdev = alloc_etherdev(sizeof(*priv));
-	if(!netdev) {
-		printk(KERN_INFO "%s: mem alloc failed.\n", DRV_NAME);
-		return -ENOMEM;
-	}
-	priv = netdev_priv(netdev);
-	priv->pdev = pdev;
-	priv->netdev = netdev;
-	pci_set_drvdata(pdev, netdev);
-
-	/* pci initialization */
-	if(ccat_eth_init_pci(priv)) {
-		printk(KERN_INFO "%s: CCAT pci init failed.\n", DRV_NAME);
-		ccat_eth_remove_one(priv->pdev);		
-		return -EIO;
-	}
-	SET_NETDEV_DEV(netdev, &pdev->dev);
-
-	/* complete ethernet device initialization */
-	memcpy_fromio(netdev->dev_addr, priv->reg.mii + 8, 6); /* init MAC address */
-	netdev->netdev_ops = &ccat_eth_netdev_ops;
-	priv->rx_thread = kthread_run(run_rx_thread, priv->netdev, "%s_rx", DRV_NAME);
-	priv->tx_thread = kthread_run(run_tx_thread, priv->netdev, "%s_tx", DRV_NAME);
-	if(0 != register_netdev(netdev)) {
-		printk(KERN_INFO "%s: unable to register network device.\n", DRV_NAME);
-		ccat_eth_remove_one(pdev);
-		return -EINVAL;
-	}
-	printk(KERN_INFO "%s: registered %s as network device.\n", DRV_NAME, netdev->name);
-	return 0;
+	memcpy_fromio(&priv->info, addr, sizeof(priv->info));
+	ccat_eth_priv_init_mappings(priv);
+	ccat_print_function_info(priv);
+	return ccat_eth_priv_init_dma(priv);
 }
 
-static int ccat_eth_init_pci(struct ccat_eth_priv *priv)
+int ccat_eth_init_netdev(struct net_device *dev)
 {
-	struct pci_dev *pdev = priv->pdev;
-	void __iomem *addr;
-	size_t i;
-	int status;
-	uint8_t revision;
-	uint8_t num_functions;
-	status = pci_enable_device (pdev);
-	if(status) {
-		printk(KERN_INFO "%s: enable CCAT pci device %s failed with %d\n", DRV_NAME, pdev->dev.kobj.name, status);
-		return status;
-	}
-	
-	pci_set_master(pdev);
-	status = pci_read_config_byte(pdev, PCI_REVISION_ID, &revision);
-	if(status) {
-		printk(KERN_INFO "%s: read CCAT pci revision failed with %d\n", DRV_NAME, status);
-		return status;
-	}
-	
-	/* FIXME upgrade to a newer kernel to get support of dma_set_mask_and_coherent()
-	if (!dma_set_mask_and_coherent(&dev->dev, DMA_BIT_MASK(64))) { */
-	//if (!dma_set_mask(&pdev->dev, DMA_BIT_MASK(64))) {
-	if (!dma_set_mask(&pdev->dev, DMA_BIT_MASK(64))) {
-		printk(KERN_INFO "%s: 64 bit DMA supported.\n", DRV_NAME);
-	/*} else if (!dma_set_mask_and_coherent(&dev->dev, DMA_BIT_MASK(32))) { */
-	} else if (!dma_set_mask(&pdev->dev, DMA_BIT_MASK(32))) {
-		printk(KERN_INFO "%s: 32 bit DMA supported.\n", DRV_NAME);
-	} else {
-		printk(KERN_WARNING "%s: No suitable DMA available.\n", DRV_NAME);
-	}
-	
-	if(ccat_bar_init(&priv->bar[0], 0, priv->pdev)) {
-		printk(KERN_WARNING "%s: initialization of bar0 failed.\n", DRV_NAME);
-		return -EIO;
-	}
-	
-	if(ccat_bar_init(&priv->bar[2], 2, priv->pdev)) {
-		printk(KERN_WARNING "%s: initialization of bar2 failed.\n", DRV_NAME);
-		return -EIO;
-	}
-	
-	num_functions = ioread8(priv->bar[0].ioaddr + 4); /* jump to CCatInfoBlock.nMaxEntries */
-	
-	/* find CCATINFO_ETHERCAT_MASTER_DMA function */
-	for(i = 0, addr = priv->bar[0].ioaddr; i < num_functions; ++i, addr += sizeof(priv->info)) {
-		if(CCATINFO_ETHERCAT_MASTER_DMA == ioread16(addr)) {
-			memcpy_fromio(&priv->info, addr, sizeof(priv->info));
-			ccat_eth_priv_init_mappings(priv);
-			ccat_print_function_info(priv);
-			status = ccat_eth_priv_init_dma(priv);
-			break;
-		}
-	}
-	return status;
+	struct ccat_eth_priv *const priv = netdev_priv(dev);
+	memcpy_fromio(dev->dev_addr, priv->reg.mii + 8, 6); /* init MAC address */
+	dev->netdev_ops = &ccat_eth_netdev_ops;
+	priv->rx_thread = kthread_run(run_rx_thread, priv->netdev, "%s_rx", DRV_NAME);
+	priv->tx_thread = kthread_run(run_tx_thread, priv->netdev, "%s_tx", DRV_NAME);
+	return register_netdev(dev);
 }
 
 static int ccat_eth_open(struct net_device *dev)
@@ -358,32 +269,16 @@ static void ccat_eth_receive(struct net_device *const dev, const struct ccat_eth
 	netif_rx(skb);
 }
 
-static void ccat_eth_remove_one(struct pci_dev *pdev)
+void ccat_eth_remove(struct net_device *const netdev)
 {
-	struct net_device *const netdev = pci_get_drvdata(pdev);
-	if(netdev) {
-		struct ccat_eth_priv *const priv = netdev_priv(netdev);
-		if(priv->rx_thread) {
-			kthread_stop(priv->rx_thread);
-		}
-		unregister_netdev(netdev);
-		if(priv->tx_thread) {
-			kthread_stop(priv->tx_thread);
-		}
-		ccat_eth_remove_pci(priv);
-		free_netdev(netdev);
-		pci_disable_device (pdev);
-		printk(KERN_INFO "%s: cleanup done.\n\n", DRV_NAME);
+	struct ccat_eth_priv *const priv = netdev_priv(netdev);
+	if(priv->rx_thread) {
+		kthread_stop(priv->rx_thread);
 	}
-}
-
-static void ccat_eth_remove_pci(struct ccat_eth_priv *priv)
-{
-	ccat_dma_free(&priv->tx_fifo.dma);
-	ccat_dma_free(&priv->rx_fifo.dma);
-	ccat_bar_free(&priv->bar[2]);
-	ccat_bar_free(&priv->bar[0]);
-	priv->pdev = NULL;
+	unregister_netdev(netdev);
+	if(priv->tx_thread) {
+		kthread_stop(priv->tx_thread);
+	}
 }
 
 static netdev_tx_t ccat_eth_start_xmit(struct sk_buff *skb, struct net_device *dev)
@@ -567,26 +462,3 @@ static int run_tx_thread(void *data)
 	printk(KERN_INFO "%s: %s() stopped.\n", DRV_NAME, __FUNCTION__);
 	return 0;
 }
-
-static struct pci_driver pci_driver = {
-	.name = DRV_NAME,
-	.id_table = pci_ids,
-	.probe = ccat_eth_init_one,
-	.remove = ccat_eth_remove_one,
-};
-
-static void ccat_eth_exit_module(void) {
-	pci_unregister_driver(&pci_driver);
-}
-
-static int ccat_eth_init_module(void) {
-	BUILD_BUG_ON(sizeof(struct ccat_eth_frame) != sizeof(CCatDmaTxFrame));
-	BUILD_BUG_ON(sizeof(struct ccat_eth_frame) != sizeof(CCatRxDesc));
-	BUILD_BUG_ON(offsetof(struct ccat_eth_frame, data) != offsetof(CCatDmaTxFrame, data));
-	BUILD_BUG_ON(offsetof(struct ccat_eth_frame, data) != offsetof(CCatRxDesc, data));
-	pr_info("%s, %s\n", DRV_DESCRIPTION, DRV_VERSION);
-	return pci_register_driver(&pci_driver);
-}
-
-module_exit(ccat_eth_exit_module);
-module_init(ccat_eth_init_module);
