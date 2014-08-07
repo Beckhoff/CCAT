f9aa89567652a608b76b98d62e610f9a94d98754
Patrick Brünn <p.bruenn@beckhoff.com>
2014-08-07 12:12:07 +0200

    adjust gpio test script for slower CX5000

--- unittest/test_gpio.sh
+++ unittest/test_gpio.sh
@@ -2,7 +2,10 @@ DURATION=3
 
 sh rw_gpio_all.sh
 sleep 1
-for i in $(seq 244 1 252)
+# CX2030
+#for i in $(seq 244 1 252)
+# CX5000
+for i in $(seq 232 1 255)
 do
 sh toggle_gpio.sh ${i} ${DURATION} &
 done
