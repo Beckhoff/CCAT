d6eb170ffd96ea3a81eafe863f5f69e3f195ae5e
Patrick Brünn <p.bruenn@beckhoff.com>
2014-08-07 12:12:07 +0200

    adjust gpio test script for slower CX5000

--- unittest/toggle_gpio.sh
+++ unittest/toggle_gpio.sh
@@ -3,9 +3,9 @@ DURATION=${2}
 FREQUENZY=0.25
 
 echo $GPIO > /sys/class/gpio/export
-sleep 1
+sleep 2
 echo "out" > /sys/class/gpio/gpio${GPIO}/direction
-sleep 1
+sleep 2
 for i in $(seq 1 1 ${DURATION})
 do
 	echo 1 > /sys/class/gpio/gpio${GPIO}/value
