34b21aa287043dfa252a488477640858243cce78
Patrick Brünn <p.bruenn@beckhoff.com>
2015-10-02 16:04:59 +0200

    add CX2030 test results

--- unittest/rw_gpio_all.sh
+++ unittest/rw_gpio_all.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-for i in $(seq 244 1 251)
+if [ $# -ne 1 ]; then
+	echo "Usage $0 <gpio_base>\n      $0 244"
+	exit -1
+fi
+
+BASE=$1
+#on CX2030
+NUM=11
+
+for i in $(seq ${BASE} 1 $((BASE+NUM)))
 do
 sh rw_gpio.sh ${i} &
 done
-sh rw_gpio.sh 252
