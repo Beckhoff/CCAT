08ca11a86d3b2ef52684a31367f9d859e3374bee
Patrick Brünn <p.bruenn@beckhoff.com>
2015-10-26 15:20:15 +0100

    print registerd gpio base to kernel log

--- gpio.c
+++ gpio.c
@@ -141,8 +141,8 @@ static int ccat_gpio_probe(struct ccat_function *func)
 		kfree(gpio);
 		return ret;
 	}
-	pr_info("registered %s as gpio chip with #%d GPIOs.\n",
-		gpio->chip.label, gpio->chip.ngpio);
+	pr_info("registered %s as gpiochip%d with #%d GPIOs.\n",
+		gpio->chip.label, gpio->chip.base, gpio->chip.ngpio);
 	func->private_data = gpio;
 	return 0;
 }
