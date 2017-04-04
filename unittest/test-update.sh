e5affd9a4e67c06cdb5ce5f5c748fb6d7b56ee9e
Patrick Bruenn <p.bruenn@beckhoff.com>
2017-04-04 15:21:45 +0200

    fmt: unittest: update and sram test scripts

--- unittest/test-update.sh
+++ unittest/test-update.sh
@@ -9,9 +9,9 @@ mac=$(cat /sys/class/net/${interface}/address | sed -r 's/:/-/g')
 dev_type=update
 dev_count=$(ls /dev/ccat_${dev_type}* | wc -l)
 
-for ((dev_slot=0; dev_slot<${dev_count}; dev_slot++)) {
+for ((dev_slot = 0; dev_slot < ${dev_count}; dev_slot++)); do
 	dev_file=/dev/ccat_${dev_type}${dev_slot}
 	ref_file=${script_path}/${dev_type}${dev_slot}.bin.ref-${mac}
 
 	${script_path}/../scripts/update_ccat.sh ${dev_file} ${ref_file}
-}
+done
