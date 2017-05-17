74448b9798196a8d8ff449cb5aa16db657c228d6
Patrick Bruenn <p.bruenn@beckhoff.com>
2017-05-17 12:38:56 +0200

    unittest: fix test-all.sh

--- unittest/test-all.sh
+++ unittest/test-all.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -l
 
-if [ $# -ne 2]; then
+set -e
+
+if [ $# -ne 2 ]; then
 	echo "Usage: $0 <local_ip> <server_ip>"
 	exit -1
 fi
 
-./unittest/test-network.sh "$1" "$2" &&
-./unittest/test-systemtime.sh &&
-./unittest/test-rw_cdev.sh sram 131072 &&
-./unittest/test-update.sh &&
+./unittest/test-network.sh "$1" "$2"
+./unittest/test-systemtime.sh
+./unittest/test-rw_cdev.sh sram 131072
+./unittest/test-update.sh
 echo "All tests completed"
