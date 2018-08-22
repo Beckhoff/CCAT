29cfef5e629686a4483aa3ca5fb1503e824de997
Patrick Bruenn <p.bruenn@beckhoff.com>
2018-08-22 12:09:57 +0200

    Relicense CCAT driver under MIT license

--- module.c
+++ module.c
@@ -1,21 +1,8 @@
+// SPDX-License-Identifier: MIT
 /**
     Network Driver for Beckhoff CCAT communication controller
-    Copyright (C) 2014-2015  Beckhoff Automation GmbH
+    Copyright (C) 2014-2018  Beckhoff Automation GmbH & Co. KG
     Author: Patrick Bruenn <p.bruenn@beckhoff.com>
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 #include <linux/etherdevice.h>
@@ -27,7 +14,7 @@
 
 MODULE_DESCRIPTION(DRV_DESCRIPTION);
 MODULE_AUTHOR("Patrick Bruenn <p.bruenn@beckhoff.com>");
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL and additional rights");
 MODULE_VERSION(DRV_VERSION);
 
 static struct ccat_cell ccat_cells[] = {
@@ -117,7 +104,7 @@ loff_t ccat_cdev_llseek(struct file * f, loff_t offset, int whence)
 	return fixed_size_llseek(f, offset, whence, iosize);
 }
 
-EXPORT_SYMBOL_GPL(ccat_cdev_llseek);
+EXPORT_SYMBOL(ccat_cdev_llseek);
 
 static int ccat_cdev_init(struct cdev *cdev, dev_t dev, struct class *class,
 			  struct file_operations *fops)
@@ -162,7 +149,7 @@ int ccat_cdev_open(struct inode *const i, struct file *const f)
 	return 0;
 }
 
-EXPORT_SYMBOL_GPL(ccat_cdev_open);
+EXPORT_SYMBOL(ccat_cdev_open);
 
 int ccat_cdev_probe(struct ccat_function *func, struct ccat_class *cdev_class,
 		    size_t iosize)
@@ -187,7 +174,7 @@ int ccat_cdev_probe(struct ccat_function *func, struct ccat_class *cdev_class,
 	return 0;
 }
 
-EXPORT_SYMBOL_GPL(ccat_cdev_probe);
+EXPORT_SYMBOL(ccat_cdev_probe);
 
 int ccat_cdev_release(struct inode *const i, struct file *const f)
 {
@@ -199,7 +186,7 @@ int ccat_cdev_release(struct inode *const i, struct file *const f)
 	return 0;
 }
 
-EXPORT_SYMBOL_GPL(ccat_cdev_release);
+EXPORT_SYMBOL(ccat_cdev_release);
 
 int ccat_cdev_remove(struct platform_device *pdev)
 {
@@ -212,7 +199,7 @@ int ccat_cdev_remove(struct platform_device *pdev)
 	return 0;
 }
 
-EXPORT_SYMBOL_GPL(ccat_cdev_remove);
+EXPORT_SYMBOL(ccat_cdev_remove);
 
 static int ccat_function_connect(struct ccat_function
 				 *const func, struct ccat_device *const ccatdev)
--- systemtime.c
+++ systemtime.c
@@ -1,21 +1,8 @@
+// SPDX-License-Identifier: MIT
 /**
-Systemtime Driver for Beckhoff CCAT communication controller
-Copyright (C) 2016  Beckhoff Automation GmbH
-Author: Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    Systemtime Driver for Beckhoff CCAT communication controller
+    Copyright (C) 2016 - 2018 Beckhoff Automation GmbH & Co. KG
+    Author: Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>
 */
 
 #include <linux/clocksource.h>
@@ -104,5 +91,5 @@ module_platform_driver(systemtime_driver);
 
 MODULE_DESCRIPTION(DRV_DESCRIPTION);
 MODULE_AUTHOR("Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>");
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL and additional rights");
 MODULE_VERSION(DRV_VERSION);
