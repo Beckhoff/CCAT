e5aab736e87288205e622278edc4d17ca3ddb1cf
Patrick Bruenn <p.bruenn@beckhoff.com>
2025-08-28 12:39:12 +0200

    tests/ccat-module: nuke all pci driver_overrides
    If we want to run our test in a loop (BHF_CI_LOOP=y). We have to
    reenable the TcSystemService afterwards. The problem is it will add a
    driver_override to our CCAT device(even if it was missing in the first
    run). To get rid of it without awk, grep and find gymnastics, we simply
    drop _ALL_ driver_overrides. This can have serious sideeffects!
    
    However, since we only trigger the TC/LUR test pipeline with our own
    single test, we don't care at the moment and prefer a simple approach
    over some complicated search logic.
    
    One nice sideeffect of this is that we found the original problem with
    CX52x0. Yeah, there the driver_override was set in the first round
    already, so with our unconditional cleanup, we can now merge CX52x0 back
    into the normal control flow.
    
    Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

--- .test-stage/tests/ccat-module/setup.sh
+++ .test-stage/tests/ccat-module/setup.sh
@@ -8,29 +8,17 @@ set -u
 readonly test_device="${1?Missing <test-device>}"
 readonly ssh_cmd="ssh Administrator@${test_device}"
 
-family="$(rackctl-config get test-device family)"
-readonly family
-case "${family}" in
-	CX52x0)
-		# On CX52x0 it is not enough to unload vfio-pci to be able to
-		# bind the standalone ccat driver. Instead we have to disable
-		# both TcSysConf and the systemservice and restart.
-		${ssh_cmd} /bin/sh -eu <<- EOF
-			sudo systemctl disable TcSystemServiceUm
-			sudo systemctl disable TcSysConf
-		EOF
-		ssh_reboot "Administrator@${test_device}"
-		;;
-	*)
-		# Stop TwinCAT and unload the vfio-pci driver, so we can use
-		# our driver instead.
-		${ssh_cmd} /bin/sh -eu <<- EOF
-			sudo systemctl stop TcSystemServiceUm
-			# Sometimes (e.g.: with BHF_CI_LOOP=y) the driver was
-			# already unloaded.
-			if lsmod | grep vfio-pci; then
-				sudo rmmod vfio-pci
-			fi
-		EOF
-		;;
-esac
+# Stop TwinCAT and unload the vfio-pci driver, so we can use
+# our driver instead.
+${ssh_cmd} /bin/sh -eu <<- EOF
+	sudo systemctl stop TcSystemServiceUm
+	# Sometimes (e.g.: with BHF_CI_LOOP=y) the driver was
+	# already unloaded.
+	if ! awk '\$1 == "vfio_pci" { exit 1 }' /proc/modules; then
+		sudo rmmod vfio_pci
+	fi
+	# YEAH, this is brutal, but we don't care! TcSysConf "forgets"
+	# to cleanup driver_override for our CCAT. So to keep this
+	# easy here we just overwrite ALL overrides...
+	echo "" | sudo tee /sys/bus/pci/devices/*/driver_override
+EOF
