Beckhoff/CCAT

commit 7f053c8653b686cc0eb6fe256910daf93c6654fe

netdev: Switch to use hrtimers_setup()
Commit 9779489a31d77a7b9cb6f20d2d2caced4e29dbe6 in the Linux kernel
deleted the hrtimer_init() function since all intree users were
converted to hrtimer_setup(), which was introduced in kernel v6.13 with
commit 908a1d775422ba2e27a5e33d0c130b522419e121.

So here we go and do our homework a bit late again...

Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

netdev.c
@@ -772,12 +772,19 @@ static void ccat_eth_get_stats64(struct net_device *dev,
 #endif
 }
 
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6,13,0))
+static void hrtimer_setup(struct hrtimer *timer, enum hrtimer_restart (*function)(struct hrtimer *), clockid_t clock_id, enum hrtimer_mode mode)
+{
+	hrtimer_init(poll_timer, clock_id, mode);
+	poll_timer->function = hrtimer_restart;
+}
+#endif
+
 static int ccat_eth_open(struct net_device *dev)
 {
 	struct ccat_eth_priv *const priv = netdev_priv(dev);
 
-	hrtimer_init(&priv->poll_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
-	priv->poll_timer.function = poll_timer_callback;
+	hrtimer_setup(&priv->poll_timer, poll_timer_callback, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	hrtimer_start(&priv->poll_timer, POLL_TIME, HRTIMER_MODE_REL);
 	return 0;
 }

commit e247407382fd3c47abdd961713cc563e11f8b63c

netdev: fix build for LINUX_VERSION_CODE < KERNEL_VERSION(6,13,0)
Commit 7f053c8653b686cc0eb6fe256910daf93c6654fe wasn't even compile
tested. Shame on the commiter!

Fixes: 7f053c8653b686cc0eb6fe256910daf93c6654fe
Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

netdev.c
@@ -775,8 +775,8 @@ static void ccat_eth_get_stats64(struct net_device *dev,
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(6,13,0))
 static void hrtimer_setup(struct hrtimer *timer, enum hrtimer_restart (*function)(struct hrtimer *), clockid_t clock_id, enum hrtimer_mode mode)
 {
-	hrtimer_init(poll_timer, clock_id, mode);
-	poll_timer->function = hrtimer_restart;
+	hrtimer_init(timer, clock_id, mode);
+	timer->function = function;
 }
 #endif
 