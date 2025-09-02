4041a07a4cd3039eb82172af9367b407e01c8dab
Patrick Bruenn <p.bruenn@beckhoff.com>
2025-09-02 11:30:32 +0000

    test-connectivity: import cleanup from teardown
    We should keep setup.sh and teardown.sh symmetric. With this it is much
    easier to run test-connectivity manually in a loop, too.
    
    Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

--- unittest/test-connectivity.sh
+++ unittest/test-connectivity.sh
@@ -9,6 +9,9 @@ setup_interface() {
 	ip link set "${_interface}" netns "ns${_ns_number}"
 	ip netns exec "ns${_ns_number}" ip addr add "192.168.1.${_ns_number}"/24 dev "${_interface}"
 	ip netns exec "ns${_ns_number}" ip link set "${_interface}" up
+	"${CLEANUP}/add" "ip netns exec ns${_ns_number} ip neigh"
+	"${CLEANUP}/add" "ip netns exec ns${_ns_number} ip --statistics --statistics link show dev ${_interface}"
+	"${CLEANUP}/add" "ip netns exec ns${_ns_number} cat /proc/net/dev"
 }
 
 run_iperf() {
@@ -23,10 +26,15 @@ run_iperf() {
 set -e
 set -u
 
+eval "$(cleanup init)"
+
 readonly rte0="${1?Missing <rte0>}"
 
 ip netns add ns1
+"${CLEANUP}/add" "ip netns del ns1"
+
 ip netns add ns2
+"${CLEANUP}/add" "ip netns del ns2"
 
 setup_interface eth0 1
 setup_interface "${rte0}" 2
@@ -36,6 +44,7 @@ ip netns exec ns2 iperf3 \
 	--daemon \
 	--pidfile=/tmp/iperf3.pid \
 	--server
+"${CLEANUP}/add" "pkill --pidfile=/tmp/iperf3.pid"
 
 run_iperf
 run_iperf --reverse
