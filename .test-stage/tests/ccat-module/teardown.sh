bef133a28240c24590c2333caf304a51806a4fc5
Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>
2025-12-09 11:31:09 +0100

    teardown.sh: unload ccat driver
    On devices where ccat is used for twincat leds we'll fail on TwinCAT
    startup otherwise.

--- .test-stage/tests/ccat-module/teardown.sh
+++ .test-stage/tests/ccat-module/teardown.sh
@@ -11,6 +11,13 @@ readonly ssh_cmd="ssh Administrator@${test_device}"
 readonly rte0="$(rackctl-config get test-device rte0/debian)"
 
 ${ssh_cmd} sudo /bin/sh -$- <<- EOF
+		# unload ccat so twincat can access it again
+		rmmod ccat_update
+		rmmod ccat_systemtime
+		rmmod ccat_sram
+		rmmod ccat_gpio
+		rmmod ccat_netdev
+		rmmod ccat
 		# Turn TwinCAT back on again so the generic_teardown doesn't fail
 		systemctl --now enable TcSysConf TcSystemServiceUm
 EOF
