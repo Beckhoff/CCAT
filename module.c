Beckhoff/CCAT

commit 7c1a02c3278504f9b01e5659cf3bea9a60a1d6c3

all: platform_driver::remove() returns void since v6.11
On kernel versions >= 6.11, platform_driver::remove() returns void
instead of the integer exit code, which was ignored [1].
To stay backwards compatible we introduce two new macros for this.

[1] https://lore.kernel.org/lkml/2024060432-relieving-yonder-85ae@gregkh/T/

Reviewed-by: Patrick Bruenn <p.bruenn@beckhoff.com>
Signed-off-by: Filip Jöde <F.Joede@beckhoff.com>

module.c
@@ -192,7 +192,7 @@ int ccat_cdev_release(struct inode *const i, struct file *const f)
 
 EXPORT_SYMBOL(ccat_cdev_release);
 
-int ccat_cdev_remove(struct platform_device *pdev)
+REMOVE_RESULT ccat_cdev_remove(struct platform_device *pdev)
 {
 	struct ccat_function *const func = pdev->dev.platform_data;
 	struct ccat_cdev *const ccdev = func->private_data;
@@ -200,7 +200,7 @@ int ccat_cdev_remove(struct platform_device *pdev)
 	cdev_del(&ccdev->cdev);
 	device_destroy(ccdev->class->class, ccdev->dev);
 	free_ccat_cdev(ccdev);
-	return 0;
+	return REMOVE_OK;
 }
 
 EXPORT_SYMBOL(ccat_cdev_remove);

gpio.c
@@ -140,13 +140,13 @@ static int ccat_gpio_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static int ccat_gpio_remove(struct platform_device *pdev)
+static REMOVE_RESULT ccat_gpio_remove(struct platform_device *pdev)
 {
 	struct ccat_function *const func = pdev->dev.platform_data;
 	struct ccat_gpio *const gpio = func->private_data;
 
 	gpiochip_remove(&gpio->chip);
-	return 0;
+	return REMOVE_OK;
 }
 
 static struct platform_driver gpio_driver = {

module.h
@@ -27,6 +27,7 @@
 #include <linux/kernel.h>
 #include <linux/pci.h>
 #include <linux/mfd/core.h>
+#include <linux/version.h>
 
 #define DRV_EXTRAVERSION ""
 #define DRV_VERSION      "0.16" DRV_EXTRAVERSION
@@ -34,6 +35,14 @@
 
 #undef pr_fmt
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 11, 0)
+#define REMOVE_RESULT int
+#define REMOVE_OK 0
+#else
+#define REMOVE_RESULT void
+#define REMOVE_OK
+#endif
 
 /**
  * CCAT function type identifiers (u16)
@@ -140,7 +149,7 @@ struct ccat_class {
 	struct file_operations fops;
 };
 
-extern int ccat_cdev_remove(struct platform_device *pdev);
+extern REMOVE_RESULT ccat_cdev_remove(struct platform_device *pdev);
 extern int ccat_cdev_probe(struct ccat_function *func,
 			   struct ccat_class *cdev_class, size_t iosize);
 

netdev.c
@@ -865,14 +865,14 @@ static int ccat_eth_dma_probe(struct platform_device *pdev)
 	return ccat_eth_init_netdev(priv);
 }
 
-static int ccat_eth_dma_remove(struct platform_device *pdev)
+static REMOVE_RESULT ccat_eth_dma_remove(struct platform_device *pdev)
 {
 	struct ccat_function *const func = pdev->dev.platform_data;
 	struct ccat_eth_priv *const eth = func->private_data;
 	unregister_netdev(eth->netdev);
 	ccat_eth_priv_free(eth);
 	free_netdev(eth->netdev);
-	return 0;
+	return REMOVE_OK;
 }
 
 static struct platform_driver ccat_eth_dma_driver = {
@@ -899,14 +899,14 @@ static int ccat_eth_eim_probe(struct platform_device *pdev)
 	return ccat_eth_init_netdev(priv);
 }
 
-static int ccat_eth_eim_remove(struct platform_device *pdev)
+static REMOVE_RESULT ccat_eth_eim_remove(struct platform_device *pdev)
 {
 	struct ccat_function *const func = pdev->dev.platform_data;
 	struct ccat_eth_priv *const eth = func->private_data;
 	unregister_netdev(eth->netdev);
 	ccat_eth_priv_free(eth);
 	free_netdev(eth->netdev);
-	return 0;
+	return REMOVE_OK;
 }
 
 static struct platform_driver ccat_eth_eim_driver = {

systemtime.c
@@ -72,13 +72,13 @@ static int ccat_systemtime_probe(struct platform_device *pdev)
 	return clocksource_register_hz(&systemtime->clock, NSEC_PER_SEC);
 }
 
-static int ccat_systemtime_remove(struct platform_device *pdev)
+static REMOVE_RESULT ccat_systemtime_remove(struct platform_device *pdev)
 {
 	struct ccat_function *const func = pdev->dev.platform_data;
 	struct ccat_systemtime *const systemtime = func->private_data;
 
 	clocksource_unregister(&systemtime->clock);
-	return 0;
+	return REMOVE_OK;
 };
 
 static struct platform_driver systemtime_driver = {

commit 9bca68fa75431f60807e78709f3e473752291d24

module: ccat_platform_remove needs to return void since 6.11
This was missed because it was hidden by CONFIG_PCI.
Fixes: 7c1a02c32785 ("all: platform_driver::remove() returns void since v6.11")

module.c
@@ -392,7 +392,7 @@ static int ccat_platform_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static int ccat_platform_remove(struct platform_device *pdev)
+static REMOVE_RESULT ccat_platform_remove(struct platform_device *pdev)
 {
 	struct ccat_device *ccatdev = platform_get_drvdata(pdev);
 
@@ -401,7 +401,7 @@ static int ccat_platform_remove(struct platform_device *pdev)
 		iounmap(ccatdev->bar_0);
 		release_mem_region(CCAT_EIM_ADDR, CCAT_EIM_LEN);
 	}
-	return 0;
+	return REMOVE_OK;
 }
 
 static const struct of_device_id bhf_platform_ccat_ids[] = {